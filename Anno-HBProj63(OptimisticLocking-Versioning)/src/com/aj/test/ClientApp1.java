770e8081d04307b2271d38f4675e7745c4e4aea7
Ajay <ajaykumark0213@gmail.com>
2017-07-09 16:41:25 +0530

    New Poc on hibernte

---
+++ Anno-HBProj63(OptimisticLocking-Versioning)/src/com/aj/test/ClientApp1.java
@@ -0,0 +1,41 @@
+package com.aj.test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.aj.domain.Student;
+import com.aj.utility.HibernateUtil;
+
+public class ClientApp1 {
+
+	public static void main(String[] args) {
+		Session ses=null;
+		Student st=null;
+		Transaction tx=null;
+		//get Session
+		ses=HibernateUtil.getSession();
+		//load object
+		st=(Student)ses.get(Student.class,101);
+		try{
+		  tx=ses.beginTransaction();
+		     st.setAddress("hyd15");
+		    ses.update(st);
+		    System.out.println("Client1 is Sleep state ....");
+		    Thread.sleep(30000);
+		    System.out.println("Client1 got back normal state");
+		  tx.commit();
+		   
+		}//try
+		catch(Exception e){
+			tx.rollback();
+			e.printStackTrace();
+		}
+		System.out.println("Version---->"+st.getVer());
+
+		
+      //close Session
+		HibernateUtil.closeSession();
+		//close SessionFactory
+		HibernateUtil.closeSessionFactory();
+	}//main
+}//class
